825387eb01eebe1f12a476c3143eeabbce67af24
Nikola Stojiljkovic <xection@gmail.com>
2018-01-17 12:14:28 +0100

    [TASK] Publish signed artifacts on release
    Change-Id: I894186de68639b89b39decf50c1677eb978a264f
    Related: #WC-27068, #WC-27081

--- project/Publish.scala
+++ project/Publish.scala
@@ -1,4 +1,7 @@
-import sbt._, Keys._
+import sbt._
+import Keys._
+import sbtrelease.ReleasePlugin.autoImport.{ReleaseStep, releaseProcess, releaseStepCommand}
+import sbtrelease.ReleaseStateTransformations._
 
 object Publish {
 
@@ -11,26 +14,39 @@ object Publish {
       else
         Some("releases" at nexus + "service/local/staging/deploy/maven2")
     },
+    releaseProcess := Seq[ReleaseStep](
+      checkSnapshotDependencies,
+      inquireVersions,
+      runClean,
+      setReleaseVersion,
+      commitReleaseVersion,
+      tagRelease,
+      releaseStepCommand("publishSigned"),
+      setNextVersion,
+      commitNextVersion,
+      releaseStepCommand("sonatypeReleaseAll"),
+      pushChanges
+    ),
     pomExtra := (
       <url>https://github.com/nstojiljkovic/play-swagger</url>
-      <licenses>
-        <license>
-          <name>Apache License 2.0</name>
-          <url>https://raw.githubusercontent.com/nstojiljkovic/play-swagger/master/LICENSE</url>
-          <distribution>repo</distribution>
-        </license>
-      </licenses>
-      <scm>
-        <url>git@github.com:nstojiljkovic/play-swagger.git</url>
-        <connection>scm:git@github.com:nstojiljkovic/play-swagger.git</connection>
-      </scm>
-      <developers>
-        <developer>
-          <id>nstojiljkovic</id>
-          <name>Nikola Stojiljković</name>
-          <url>https://github.com/nstojiljkovic</url>
-        </developer>
-      </developers>),
+        <licenses>
+          <license>
+            <name>Apache License 2.0</name>
+            <url>https://raw.githubusercontent.com/nstojiljkovic/play-swagger/master/LICENSE</url>
+            <distribution>repo</distribution>
+          </license>
+        </licenses>
+        <scm>
+          <url>git@github.com:nstojiljkovic/play-swagger.git</url>
+          <connection>scm:git@github.com:nstojiljkovic/play-swagger.git</connection>
+        </scm>
+        <developers>
+          <developer>
+            <id>nstojiljkovic</id>
+            <name>Nikola Stojiljković</name>
+            <url>https://github.com/nstojiljkovic</url>
+          </developer>
+        </developers>),
     publishArtifact in Test := false)
 
   val sbtPluginSettings = Seq(
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -2,6 +2,10 @@ libraryDependencies += "org.scala-sbt" %% "scripted-plugin" % sbtVersion.value
 
 addSbtPlugin("com.github.gseitz" % "sbt-release" % "1.0.6")
 
+addSbtPlugin("com.jsuereth" % "sbt-pgp" % "1.1.0")
+
+addSbtPlugin("org.xerial.sbt" % "sbt-sonatype" % "2.0")
+
 addSbtPlugin("org.scalariform" % "sbt-scalariform" % "1.8.0")
 
 addSbtPlugin("org.scoverage" % "sbt-scoverage" % "1.5.1")
